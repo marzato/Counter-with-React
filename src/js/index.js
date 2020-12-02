776b714e3107ce5c04ac392b8c2f2c76864edd44
unknown <marzato32@gmail.com>
2020-12-02 09:50:13 -0400

    Ready Counter-with-React

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -9,8 +10,40 @@ import "bootstrap";
 import "../styles/index.scss";
 
 function SimpleCounter(props) {
-	return <p>Hello World</p>;
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<i className="far fa-clock" />
+			</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
 }
+SimpleCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
 
-//render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
